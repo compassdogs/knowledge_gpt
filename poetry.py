3fc574ed34106bdd1e25275a51d67f69525ffb2e
compassdogs <135496494+compassdogs@users.noreply.github.com>
2023-07-10 23:29:29 -0700

    Update poetry.py

--- poetry.py
+++ poetry.py
@@ -4,7 +4,6 @@ import subprocess
 import sys
 
 def setup():
-    subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
     subprocess.run([sys.executable, "-m", "knowledge_gpt.main"])
 
 if __name__ == "__main__":
